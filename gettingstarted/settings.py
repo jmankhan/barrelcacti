33b2fa8cfac934998b40d3338829c7e5dddfa855
jalal <jmankhan1@gmail.com>
2016-08-09 19:19:08 -0400

    added basic template and styling

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -18,6 +18,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import time
 from .models import Greeting
 
 # Create your views here.
@@ -8,6 +8,19 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+
+# Stream music over http
+def stream_music(request, song):
+	print song
+	resp = StreamingHttpResponse(stream_music_generator())
+	return resp
+
+def stream_music_generator():
+	yield '<p>Test Stream</p>'
+	for x in range(1, 11):
+		yield '<div>Test %s</div>' % x
+		yield ' ' * 1024
+		time.sleep(1)
 
 def db(request):
 
