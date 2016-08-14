be263a82ed19c3d25a85d6258f8d8242684d5775
jalal <jmankhan1@gmail.com>
2016-08-14 17:58:07 -0400

    modified db settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -84,12 +84,7 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
